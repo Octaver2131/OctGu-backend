c4ba93dacdf3f7422fbace1bfd60ac8cfe58bf02
Octaver <Octaver2131@outlook.com>
2025-08-15 08:41:53 +0800

    添加 createTime、updateTime、isDelete、description字段

--- src/main/java/com/yupi/project/model/entity/Item.java
+++ src/main/java/com/yupi/project/model/entity/Item.java
@@ -1,9 +1,8 @@
 package com.yupi.project.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 import lombok.Data;
@@ -14,52 +13,69 @@ import lombok.Data;
  */
 @TableName(value ="item")
 @Data
-public class Item {
+public class Item implements Serializable {
     /**
      * 自增主键，唯一标识每条记录
      */
-    @TableId(value = "id", type = IdType.AUTO)
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
     /**
      * item名字（如周边商品名称）
      */
-    @TableField(value = "itemName")
     private String itemName;
 
     /**
      * item对应的二次元IP名称（如动漫、游戏、影视作品名）
      */
-    @TableField(value = "itemIp")
     private String itemIp;
+
+    /**
+     * 描述
+     */
+    private String description;
 
     /**
      * item种类（如徽章、手办、立牌等）
      */
-    @TableField(value = "itemCategory")
     private String itemCategory;
 
     /**
      * 单价（保留2位小数）
      */
-    @TableField(value = "unitPrice")
     private BigDecimal unitPrice;
 
     /**
      * 总价（保留2位小数）
      */
-    @TableField(value = "totalPrice")
     private BigDecimal totalPrice;
 
     /**
      * 购买数量（非负整数）
      */
-    @TableField(value = "purchaseNumber")
     private Integer purchaseNumber;
 
     /**
      * 购买时间（仅到日，格式：YYYY-MM-DD）
      */
-    @TableField(value = "purchaseTime")
     private Date purchaseTime;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
